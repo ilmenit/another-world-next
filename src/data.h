ponceto/another-world-interpreter

huge cleanup and refactoring, engine rewritten

src/data.h
@@ -1,0 +1,145 @@
+/*
+ * data.h - Copyright (c) 2004-2025
+ *
+ * Gregory Montoir, Fabien Sanglard, Olivier Poncet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __AW_DATA_H__
+#define __AW_DATA_H__
+
+// ---------------------------------------------------------------------------
+// String
+// ---------------------------------------------------------------------------
+
+struct String
+{
+    uint16_t    id    = 0xffff;
+    const char* value = nullptr;
+};
+
+// ---------------------------------------------------------------------------
+// Dictionary
+// ---------------------------------------------------------------------------
+
+struct Dictionary
+{
+    enum
+    {
+        DIC_DEFAULT = 0,
+        DIC_ENGLISH = 1,
+        DIC_FRENCH  = 2,
+    };
+
+    static const String dataEN[141];
+    static const String dataFR[141];
+};
+
+// ---------------------------------------------------------------------------
+// ResourceState
+// ---------------------------------------------------------------------------
+
+enum ResourceState
+{
+    RS_NOT_NEEDED = 0x00,
+    RS_NEEDED     = 0x01,
+    RS_LOADED     = 0x02,
+    RS_END        = 0xff,
+};
+
+// ---------------------------------------------------------------------------
+// ResourceType
+// ---------------------------------------------------------------------------
+
+enum ResourceType
+{
+    RT_SOUND    = 0x00,
+    RT_MUSIC    = 0x01,
+    RT_BITMAP   = 0x02,
+    RT_PALETTE  = 0x03,
+    RT_BYTECODE = 0x04,
+    RT_POLYGON1 = 0x05,
+    RT_POLYGON2 = 0x06,
+    RT_END      = 0xff,
+};
+
+// ---------------------------------------------------------------------------
+// Resource
+// ---------------------------------------------------------------------------
+
+struct Resource
+{
+    uint16_t id           = 0;
+    uint8_t  state        = 0xff; // offset 0x00: state
+    uint8_t  type         = 0xff; // offset 0x01: type
+    uint16_t unused1      = 0;    // offset 0x02: unused
+    uint16_t unused2      = 0;    // offset 0x04: unused
+    uint8_t  unused3      = 0;    // offset 0x06: unused
+    uint8_t  bankId       = 0;    // offset 0x07: bank id
+    uint32_t bankOffset   = 0;    // offset 0x08: bank offset
+    uint16_t unused4      = 0;    // offset 0x0c: unused
+    uint16_t packedSize   = 0;    // offset 0x0e: packed size
+    uint16_t unused5      = 0;    // offset 0x10: unused
+    uint16_t unpackedSize = 0;    // offset 0x12: unpacked size
+    uint8_t* data         = nullptr;
+};
+
+// ---------------------------------------------------------------------------
+// ResourceStats
+// ---------------------------------------------------------------------------
+
+struct ResourceStats
+{
+    uint32_t count    = 0;
+    uint32_t packed   = 0;
+    uint32_t unpacked = 0;
+};
+
+// ---------------------------------------------------------------------------
+// MemList
+// ---------------------------------------------------------------------------
+
+class MemList
+{
+public: // public interface
+    MemList(const std::string& dataDir, const std::string& dumpDir);
+
+    MemList(MemList&&) = delete;
+
+    MemList(const MemList&) = delete;
+
+    MemList& operator=(MemList&&) = delete;
+
+    MemList& operator=(const MemList&) = delete;
+
+    virtual ~MemList() = default;
+
+    auto loadMemList(Resource* resources) -> bool;
+
+    auto dumpMemList(Resource* resources) -> bool;
+
+    auto loadResource(Resource& resource) -> bool;
+
+    auto dumpResource(const Resource& resource) -> void;
+
+private: // private data
+    const std::string _dataDir;
+    const std::string _dumpDir;
+};
+
+// ---------------------------------------------------------------------------
+// End-Of-File
+// ---------------------------------------------------------------------------
+
+#endif /* __AW_DATA_H__ */

src/video.h
@@ -1,113 +1,110 @@
-/* Raw - Another World Interpreter
- * Copyright (C) 2004 Gregory Montoir
+/*
+ * video.h - Copyright (c) 2004-2025
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
+ * Gregory Montoir, Fabien Sanglard, Olivier Poncet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#ifndef __VIDEO_H__
-#define __VIDEO_H__
+#ifndef __AW_VIDEO_H__
+#define __AW_VIDEO_H__
 
 #include "intern.h"
 
-struct StrEntry {
-	uint16_t id;
-	const char *str;
+// ---------------------------------------------------------------------------
+// Video
+// ---------------------------------------------------------------------------
+
+class Video final
+    : public SubSystem
+{
+public: // public interface
+    Video(Engine& engine);
+
+    Video(Video&&) = delete;
+
+    Video(const Video&) = delete;
+
+    Video& operator=(Video&&) = delete;
+
+    Video& operator=(const Video&) = delete;
+
+    virtual ~Video() = default;
+
+    virtual auto start() -> void override final;
+
+    virtual auto reset() -> void override final;
+
+    virtual auto stop() -> void override final;
+
+public: // public video interface
+    auto setPalettes(const uint8_t* palettes, uint8_t mode) -> void;
+
+    auto selectPalette(uint8_t palette) -> void;
+
+    auto selectPage(uint8_t dst) -> void;
+
+    auto fillPage(uint8_t dst, uint8_t col) -> void;
+
+    auto copyPage(uint8_t dst, uint8_t src, int16_t vscroll) -> void;
+
+    auto blitPage(uint8_t src) -> void;
+
+    auto drawBitmap(const uint8_t* buffer) -> void;
+
+    auto drawString(uint16_t id, uint16_t x, uint16_t y, uint8_t color) -> void;
+
+    auto drawPolygons(const uint8_t* buffer, uint16_t offset, const Point& position, uint16_t zoom) -> void;
+
+private: // private interface
+    static constexpr uint8_t VIDEO_PAGE0 = 0x00;
+    static constexpr uint8_t VIDEO_PAGE1 = 0x01;
+    static constexpr uint8_t VIDEO_PAGE2 = 0x02;
+    static constexpr uint8_t VIDEO_PAGE3 = 0x03;
+    static constexpr uint8_t VIDEO_PAGEV = 0xfe;
+    static constexpr uint8_t VIDEO_PAGEI = 0xff;
+
+    static constexpr int16_t PAGE_W = 320; // page width
+    static constexpr int16_t PAGE_H = 200; // page height
+    static constexpr int16_t XMIN   =   0; // x-min
+    static constexpr int16_t YMIN   =   0; // y-min
+    static constexpr int16_t XMAX   = 319; // x-max
+    static constexpr int16_t YMAX   = 199; // y-max
+    static constexpr int16_t BPP    =   4; // bits per pixel
+    static constexpr int16_t PPB    =   2; // pixels per byte
+    static constexpr int16_t BPL    = 160; // bytes per line
+
+    auto getPage(uint8_t page) -> Page*;
+
+    auto renderString(const char* string, uint16_t x, uint16_t y, uint8_t color) -> void;
+
+    auto renderPolygon(const Polygon& polygon, const Point& position, uint16_t zoom, uint16_t color) -> void;
+
+    auto renderPolygons(const uint8_t* buffer, uint32_t offset, const Point& position, uint16_t zoom, uint8_t color) -> void;
+
+private: // private data
+    Page     _pages[4];
+    Palette  _palettes[32];
+    Page*    _page0;
+    Page*    _page1;
+    Page*    _page2;
+    Palette* _palette;
+    Polygon  _polygon;
+    uint16_t _interpolate[0x400];
 };
 
-struct Polygon {
-	enum {
-		MAX_POINTS = 50
-	};
+// ---------------------------------------------------------------------------
+// End-Of-File
+// ---------------------------------------------------------------------------
 
-	uint16_t bbw, bbh;
-	uint8_t numPoints;
-	Point points[MAX_POINTS];
-
-	void readVertices(const uint8_t *p, uint16_t zoom);
-};
-
-struct Resource;
-struct Serializer;
-struct System;
-
-// This is used to detect the end of  _stringsTableEng and _stringsTableDemo
-#define END_OF_STRING_DICTIONARY 0xFFFF 
-
-// Special value when no palette change is necessary
-#define NO_PALETTE_CHANGE_REQUESTED 0xFF 
-
-
-
-struct Video {
-	typedef void (Video::*drawLine)(int16_t x1, int16_t x2, uint8_t col);
-
-	enum {
-		VID_PAGE_SIZE  = 320 * 200 / 2
-	};
-
-	static const uint8_t _font[];
-	static const StrEntry _stringsTableEng[];
-	static const StrEntry _stringsTableDemo[];
-
-	Resource *res;
-	System *sys;
-	
-
-
-	uint8_t paletteIdRequested, currentPaletteId;
-	uint8_t *_pages[4];
-
-	// I am almost sure that:
-	// _curPagePtr1 is the work buffer
-	// _curPagePtr2 is the background buffer1
-	// _curPagePtr3 is the background buffer2
-	uint8_t *_curPagePtr1, *_curPagePtr2, *_curPagePtr3;
-
-	Polygon polygon;
-	int16_t _hliney;
-
-	//Precomputer division lookup table
-	uint16_t _interpTable[0x400];
-
-	Ptr _pData;
-	uint8_t *_dataBuf;
-
-	Video(Resource *res, System *stub);
-	void init();
-
-	void setDataBuffer(uint8_t *dataBuf, uint16_t offset);
-	void readAndDrawPolygon(uint8_t color, uint16_t zoom, const Point &pt);
-	void fillPolygon(uint16_t color, uint16_t zoom, const Point &pt);
-	void readAndDrawPolygonHierarchy(uint16_t zoom, const Point &pt);
-	int32_t calcStep(const Point &p1, const Point &p2, uint16_t &dy);
-
-	void drawString(uint8_t color, uint16_t x, uint16_t y, uint16_t strId);
-	void drawChar(uint8_t c, uint16_t x, uint16_t y, uint8_t color, uint8_t *buf);
-	void drawPoint(uint8_t color, int16_t x, int16_t y);
-	void drawLineBlend(int16_t x1, int16_t x2, uint8_t color);
-	void drawLineN(int16_t x1, int16_t x2, uint8_t color);
-	void drawLineP(int16_t x1, int16_t x2, uint8_t color);
-	uint8_t *getPage(uint8_t page);
-	void changePagePtr1(uint8_t page);
-	void fillPage(uint8_t page, uint8_t color);
-	void copyPage(uint8_t src, uint8_t dst, int16_t vscroll);
-	void copyPage(const uint8_t *src);
-	void changePal(uint8_t pal);
-	void updateDisplay(uint8_t page);
-	
-	void saveOrLoad(Serializer &ser);
-};
-
-#endif
+#endif /* __AW_VIDEO_H__ */

ilmenit/another-world-next

src/data.h
@@ -1,0 +1,145 @@
+/*
+ * data.h - Copyright (c) 2004-2025
+ *
+ * Gregory Montoir, Fabien Sanglard, Olivier Poncet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __AW_DATA_H__
+#define __AW_DATA_H__
+
+// ---------------------------------------------------------------------------
+// String
+// ---------------------------------------------------------------------------
+
+struct String
+{
+    uint16_t    id    = 0xffff;
+    const char* value = nullptr;
+};
+
+// ---------------------------------------------------------------------------
+// Dictionary
+// ---------------------------------------------------------------------------
+
+struct Dictionary
+{
+    enum
+    {
+        DIC_DEFAULT = 0,
+        DIC_ENGLISH = 1,
+        DIC_FRENCH  = 2,
+    };
+
+    static const String dataEN[141];
+    static const String dataFR[141];
+};
+
+// ---------------------------------------------------------------------------
+// ResourceState
+// ---------------------------------------------------------------------------
+
+enum ResourceState
+{
+    RS_NOT_NEEDED = 0x00,
+    RS_NEEDED     = 0x01,
+    RS_LOADED     = 0x02,
+    RS_END        = 0xff,
+};
+
+// ---------------------------------------------------------------------------
+// ResourceType
+// ---------------------------------------------------------------------------
+
+enum ResourceType
+{
+    RT_SOUND    = 0x00,
+    RT_MUSIC    = 0x01,
+    RT_BITMAP   = 0x02,
+    RT_PALETTE  = 0x03,
+    RT_BYTECODE = 0x04,
+    RT_POLYGON1 = 0x05,
+    RT_POLYGON2 = 0x06,
+    RT_END      = 0xff,
+};
+
+// ---------------------------------------------------------------------------
+// Resource
+// ---------------------------------------------------------------------------
+
+struct Resource
+{
+    uint16_t id            = 0;
+    uint8_t  state         = 0xff; // offset 0x00: state
+    uint8_t  type          = 0xff; // offset 0x01: type
+    uint16_t unused1       = 0;    // offset 0x02: unused
+    uint16_t unused2       = 0;    // offset 0x04: unused
+    uint8_t  unused3       = 0;    // offset 0x06: unused
+    uint8_t  bank_id       = 0;    // offset 0x07: bank id
+    uint32_t bank_offset   = 0;    // offset 0x08: bank offset
+    uint16_t unused4       = 0;    // offset 0x0c: unused
+    uint16_t packed_size   = 0;    // offset 0x0e: packed size
+    uint16_t unused5       = 0;    // offset 0x10: unused
+    uint16_t unpacked_size = 0;    // offset 0x12: unpacked size
+    uint8_t* data          = nullptr;
+};
+
+// ---------------------------------------------------------------------------
+// ResourceStats
+// ---------------------------------------------------------------------------
+
+struct ResourceStats
+{
+    uint32_t count    = 0;
+    uint32_t packed   = 0;
+    uint32_t unpacked = 0;
+};
+
+// ---------------------------------------------------------------------------
+// MemList
+// ---------------------------------------------------------------------------
+
+class MemList
+{
+public: // public interface
+    MemList(const std::string& dataDir, const std::string& dumpDir);
+
+    MemList(MemList&&) = delete;
+
+    MemList(const MemList&) = delete;
+
+    MemList& operator=(MemList&&) = delete;
+
+    MemList& operator=(const MemList&) = delete;
+
+    virtual ~MemList() = default;
+
+    auto loadMemList(Resource* resources) -> bool;
+
+    auto dumpMemList(Resource* resources) -> bool;
+
+    auto loadResource(Resource& resource) -> bool;
+
+    auto dumpResource(const Resource& resource) -> void;
+
+private: // private data
+    const std::string _dataDir;
+    const std::string _dumpDir;
+};
+
+// ---------------------------------------------------------------------------
+// End-Of-File
+// ---------------------------------------------------------------------------
+
+#endif /* __AW_DATA_H__ */

src/video.h
@@ -1,113 +1,110 @@
-/* Raw - Another World Interpreter
- * Copyright (C) 2004 Gregory Montoir
+/*
+ * video.h - Copyright (c) 2004-2025
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
+ * Gregory Montoir, Fabien Sanglard, Olivier Poncet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#ifndef __VIDEO_H__
-#define __VIDEO_H__
+#ifndef __AW_VIDEO_H__
+#define __AW_VIDEO_H__
 
 #include "intern.h"
 
-struct StrEntry {
-	uint16_t id;
-	const char *str;
+// ---------------------------------------------------------------------------
+// Video
+// ---------------------------------------------------------------------------
+
+class Video final
+    : public SubSystem
+{
+public: // public interface
+    Video(Engine& engine);
+
+    Video(Video&&) = delete;
+
+    Video(const Video&) = delete;
+
+    Video& operator=(Video&&) = delete;
+
+    Video& operator=(const Video&) = delete;
+
+    virtual ~Video() = default;
+
+    virtual auto start() -> void override final;
+
+    virtual auto reset() -> void override final;
+
+    virtual auto stop() -> void override final;
+
+public: // public video interface
+    auto setPalettes(const uint8_t* palettes, uint8_t mode) -> void;
+
+    auto selectPalette(uint8_t palette) -> void;
+
+    auto selectPage(uint8_t dst) -> void;
+
+    auto fillPage(uint8_t dst, uint8_t col) -> void;
+
+    auto copyPage(uint8_t dst, uint8_t src, int16_t vscroll) -> void;
+
+    auto showPage(uint8_t src) -> void;
+
+    auto drawBitmap(const uint8_t* buffer) -> void;
+
+    auto drawString(uint16_t id, uint16_t x, uint16_t y, uint8_t color) -> void;
+
+    auto drawPolygons(const uint8_t* buffer, uint16_t offset, const Point& position, uint16_t zoom) -> void;
+
+private: // private interface
+    static constexpr uint8_t VIDEO_PAGE0 = 0x00;
+    static constexpr uint8_t VIDEO_PAGE1 = 0x01;
+    static constexpr uint8_t VIDEO_PAGE2 = 0x02;
+    static constexpr uint8_t VIDEO_PAGE3 = 0x03;
+    static constexpr uint8_t VIDEO_PAGEV = 0xfe;
+    static constexpr uint8_t VIDEO_PAGEI = 0xff;
+
+    static constexpr int16_t PAGE_W = 320; // page width
+    static constexpr int16_t PAGE_H = 200; // page height
+    static constexpr int16_t XMIN   =   0; // x-min
+    static constexpr int16_t YMIN   =   0; // y-min
+    static constexpr int16_t XMAX   = 319; // x-max
+    static constexpr int16_t YMAX   = 199; // y-max
+    static constexpr int16_t BPP    =   4; // bits per pixel
+    static constexpr int16_t PPB    =   2; // pixels per byte
+    static constexpr int16_t BPL    = 160; // bytes per line
+
+    auto getPage(uint8_t page) -> Page*;
+
+    auto renderString(const char* string, uint16_t x, uint16_t y, uint8_t color) -> void;
+
+    auto renderPolygon(const Polygon& polygon, const Point& position, uint16_t zoom, uint16_t color) -> void;
+
+    auto renderPolygons(const uint8_t* buffer, uint32_t offset, const Point& position, uint16_t zoom, uint8_t color) -> void;
+
+private: // private data
+    Page     _pages[4];
+    Palette  _palettes[32];
+    Page*    _page0;
+    Page*    _page1;
+    Page*    _page2;
+    Palette* _palette;
+    Polygon  _polygon;
+    uint16_t _interpolate[0x400];
 };
 
-struct Polygon {
-	enum {
-		MAX_POINTS = 50
-	};
+// ---------------------------------------------------------------------------
+// End-Of-File
+// ---------------------------------------------------------------------------
 
-	uint16_t bbw, bbh;
-	uint8_t numPoints;
-	Point points[MAX_POINTS];
-
-	void readVertices(const uint8_t *p, uint16_t zoom);
-};
-
-struct Resource;
-struct Serializer;
-struct System;
-
-// This is used to detect the end of  _stringsTableEng and _stringsTableDemo
-#define END_OF_STRING_DICTIONARY 0xFFFF 
-
-// Special value when no palette change is necessary
-#define NO_PALETTE_CHANGE_REQUESTED 0xFF 
-
-
-
-struct Video {
-	typedef void (Video::*drawLine)(int16_t x1, int16_t x2, uint8_t col);
-
-	enum {
-		VID_PAGE_SIZE  = 320 * 200 / 2
-	};
-
-	static const uint8_t _font[];
-	static const StrEntry _stringsTableEng[];
-	static const StrEntry _stringsTableDemo[];
-
-	Resource *res;
-	System *sys;
-	
-
-
-	uint8_t paletteIdRequested, currentPaletteId;
-	uint8_t *_pages[4];
-
-	// I am almost sure that:
-	// _curPagePtr1 is the work buffer
-	// _curPagePtr2 is the background buffer1
-	// _curPagePtr3 is the background buffer2
-	uint8_t *_curPagePtr1, *_curPagePtr2, *_curPagePtr3;
-
-	Polygon polygon;
-	int16_t _hliney;
-
-	//Precomputer division lookup table
-	uint16_t _interpTable[0x400];
-
-	Ptr _pData;
-	uint8_t *_dataBuf;
-
-	Video(Resource *res, System *stub);
-	void init();
-
-	void setDataBuffer(uint8_t *dataBuf, uint16_t offset);
-	void readAndDrawPolygon(uint8_t color, uint16_t zoom, const Point &pt);
-	void fillPolygon(uint16_t color, uint16_t zoom, const Point &pt);
-	void readAndDrawPolygonHierarchy(uint16_t zoom, const Point &pt);
-	int32_t calcStep(const Point &p1, const Point &p2, uint16_t &dy);
-
-	void drawString(uint8_t color, uint16_t x, uint16_t y, uint16_t strId);
-	void drawChar(uint8_t c, uint16_t x, uint16_t y, uint8_t color, uint8_t *buf);
-	void drawPoint(uint8_t color, int16_t x, int16_t y);
-	void drawLineBlend(int16_t x1, int16_t x2, uint8_t color);
-	void drawLineN(int16_t x1, int16_t x2, uint8_t color);
-	void drawLineP(int16_t x1, int16_t x2, uint8_t color);
-	uint8_t *getPage(uint8_t page);
-	void changePagePtr1(uint8_t page);
-	void fillPage(uint8_t page, uint8_t color);
-	void copyPage(uint8_t src, uint8_t dst, int16_t vscroll);
-	void copyPage(const uint8_t *src);
-	void changePal(uint8_t pal);
-	void updateDisplay(uint8_t page);
-	
-	void saveOrLoad(Serializer &ser);
-};
-
-#endif
+#endif /* __AW_VIDEO_H__ */